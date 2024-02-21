aa95e9ee89e90e7a701618de75132dde4a8bdb34
rene-michel <rene.michel@souunit.com.br>
2024-02-20 22:07:46 -0300

    erwerwer

--- server.php
+++ server.php
@@ -4,22 +4,30 @@ use OpenSwoole\WebSocket\Server;
 use OpenSwoole\Http\Request;
 use OpenSwoole\WebSocket\Frame;
 
-$server = new Server("127.0.0.1", 9501, OpenSwoole\Server::POOL_MODE, OpenSwoole\Constant::SOCK_TCP | OpenSwoole\Constant::SSL);
-
+//$server = new Server("127.0.0.1", 9501, OpenSwoole\Server::POOL_MODE, OpenSwoole\Constant::SOCK_TCP | OpenSwoole\Constant::SSL);
+//$server = new Swoole\Server('0.0.0.0', 9501, SWOOLE_PROCESS, SWOOLE_SOCK_TCP | SWOOLE_SSL);
+$server = new Server('0.0.0.0', 9501);
 $stopHello = FALSE;
-
+/*
 $server->set([
     'open_tcp_keepalive' => true,
     'ssl_cert_file' => __DIR__ . '/config/ssl-cert-snakeoil.pem',
     'ssl_key_file' => __DIR__ . '/config/ssl-cert-snakeoil.key',
     'ssl_allow_self_signed' => true,
 ]);
-
-$server->on("Start", function(Server $server)
+*/
+$server->on("Start", function($server)
 {
     echo "OpenSwoole WebSocket Server is started at http://127.0.0.1:9501\n";
 });
-
+/*
+$server->on('Receive', function ($server, $fd, $reactor_id, $data) {
+    // Handle incoming data here
+    echo "Received data from client {$fd}: {$data}\n";
+    // You can process the received data here and send back a response if needed
+    $server->send($fd, 'Server received your message: ' . $data);
+});
+*/
 function hello($timerId, $s, $r){
     global $stopHello;
 
@@ -32,7 +40,7 @@ function hello($timerId, $s, $r){
         $s->push($r->fd, json_encode(["hello", time()]));
 }
 
-$server->on('Open', function(Server $server, OpenSwoole\Http\Request $request)
+$server->on('Open', function($server, OpenSwoole\Http\Request $request)
 {
     global $stopHello;
     $stopHello = FALSE;
@@ -40,7 +48,7 @@ $server->on('Open', function(Server $server, OpenSwoole\Http\Request $request)
     $server->tick(1000, "hello", $server, $request);
 });
 
-$server->on('Message', function(Server $server, Frame $frame)
+$server->on('Message', function($server, Frame $frame)
 {
     if($frame->opcode == 0x08)
     {
@@ -52,14 +60,14 @@ $server->on('Message', function(Server $server, Frame $frame)
     }
 });
 
-$server->on('Close', function(Server $server, int $fd)
+$server->on('Close', function($server, int $fd)
 {   
     global $stopHello;
     $stopHello = TRUE;
     echo "connection close: {$fd}\n";
 });
 
-$server->on('Disconnect', function(Server $server, int $fd)
+$server->on('Disconnect', function($server, int $fd)
 {
     echo "connection disconnect: {$fd}\n";
 });
